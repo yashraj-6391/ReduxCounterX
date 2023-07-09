488cfcd3ecd92f8aadd0ae760293359d0fcc3231
Yash_Korane <koraneyash92@gmail,com>
2023-07-09 11:09:46 +0530

    Repo Initialisation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import {incNumber,decNumber} from './actions/index';
+import './App.css'
 
-function App() {
+
+export default function App() {
+  const myState = useSelector((state)=>state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='my-container' style={{justifyContent:'center', alignItems:'center', backgroundColor:'lightgray'}}>
+      <h1 className='my-3'>Increment / Decrement Counter</h1>
+      <h4 className='my-3'>Using React-Redux</h4>
 
-export default App;
+      <div>
+        <input  className='input my-3' type="text" value={myState} ></input>
+      </div>
+      <button className='my-3 btn btn-success' title='Increment' onClick={()=>dispatch(incNumber())}>Increase</button>
+      
+      <button className='my-3 btn btn-danger' title='Decrement' onClick={()=>dispatch(decNumber())}>Decrease</button>
+      
+      
+    </div>
+  )
+}
